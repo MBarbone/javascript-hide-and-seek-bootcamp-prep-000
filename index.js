3c9d1f9fab53774ab2a0d3112e634c8f02deea21
Michael Barbone <michael.barbone@gmail.com>
2018-08-12 16:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ function nestedTarget() {
   return document.getElementById("nested").querySelector(".target");
 }
 
+
 function increaseRankBy(n) {
-  const lis = document.getElementByID("app").querySelectorAll("li.ranked-list li");
+  const lis = document.getElementByID("app").querySelectorAll("ul.ranked-list li");
   
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
   }
+}
+
+
+function deepestChild () {
+  
 }
